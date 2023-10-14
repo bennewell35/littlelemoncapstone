51ff31b01542509b478c4ca3489ea887ae85c783
Ben Newell <newelljben@gmail.com>
2023-10-14 14:47:21 -0400

    changes

--- urls.py
+++ urls.py
@@ -1,20 +1,28 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from Restaurant.views import BookingViewSet  # Replace 'yourapp' with your app's name
-from Restaurant import views
+from Restaurant.views import BookingViewSet
+from Restaurant import views as restaurant_views  # Rename the 'views' module to avoid conflicts
+from rest_framework.authtoken.views import obtain_auth_token  # Import obtain_auth_token
 
 # Define the DefaultRouter and register the BookingViewSet
 router = routers.DefaultRouter()
 router.register(r'booking', BookingViewSet)
+router.register(r'users', restaurant_views.UserViewSet)  # Register the UserViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Restaurant.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/', include(router.urls)),  # Add this for the BookingViewSet
+    path('', include('Restaurant.urls')),
+    path('api/', include(router.urls)),  # Include the BookingViewSet URLs
+    path('littlelemoncapstone/', include('littlelemoncapstoneAPI.urls')),  # Add this pattern
+    
+    # Url patterns for Djoser endpoints
+    path('menu-items/', restaurant_views.MenuItemsView.as_view(), name='menu-items-list'),
+    path('menu-items/<int:pk>/', restaurant_views.SingleMenuItemView.as_view(), name='menu-item-detail'),
+    path('message/', restaurant_views.msg, name='message'),
+    
+    # Add Djoser URL routes
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
-
-# Register the UserViewSet
-router.register(r'users', views.UserViewSet)
-
